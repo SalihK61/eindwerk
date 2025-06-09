4f4430675f192f7a7288d40b0368d7a327f8e319
komuts <komutsalih61@icloud.com>
2025-06-09 15:41:20 +0200

    requirements voor docker aanvullen. en de connecties configureren in config.py

---
+++ FileAnalyzer/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Auth0
+    AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
+    AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID")
+    AUTH0_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET")
+    AUTH0_CALLBACK_URL = os.getenv("AUTH0_CALLBACK_URL")
