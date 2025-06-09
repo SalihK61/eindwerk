6d26238c0951d362743573408c230846cf4d5bf4
komuts <komutsalih61@icloud.com>
2025-06-09 16:06:45 +0200

    de hele container en website overgeschakeld naar poort 4000 omdat poort 5000 niet beschikbaar is op mac

--- FileAnalyzer/app.py
+++ FileAnalyzer/app.py
@@ -1,20 +1,25 @@
+# app.py
+import os
 from flask import Flask
 from authlib.integrations.flask_client import OAuth
+from dotenv import load_dotenv
 from config import Config
 from models import db
 from routes import main
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # Initialize DB
+    # Initialize SQLAlchemy
     db.init_app(app)
 
-    # OAuth / Auth0
+    # Set up Auth0 via Authlib
     oauth = OAuth(app)
     auth0 = oauth.register(
-        'auth0',
+        name='auth0',
         client_id=app.config['AUTH0_CLIENT_ID'],
         client_secret=app.config['AUTH0_CLIENT_SECRET'],
         client_kwargs={'scope': 'openid profile email'},
@@ -22,12 +27,19 @@ def create_app():
     )
     app.auth0 = auth0
 
-    # Register routes
+    # Register your routes Blueprint
     app.register_blueprint(main)
+
+    # Create database tables (moet hier gebeuren anders error ps. No idea whyyy??)
+    with app.app_context():
+        db.create_all()
 
     return app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=(app.config['FLASK_ENV']=='development'))
+    # bind to all interfaces, enable debug in development
+    app.run(host='0.0.0.0',
+            port=4000,
+            debug=(app.config.get('FLASK_ENV') == 'development'))
--- FileAnalyzer/routes.py
+++ FileAnalyzer/routes.py
@@ -1,13 +1,10 @@
+# routes.py
 import os
 from flask import Blueprint, render_template, session, redirect, url_for, current_app
 from authlib.integrations.flask_client import OAuthError
 from models import db, User
 
 main = Blueprint('main', __name__)
-
-@main.before_app_first_request
-def create_tables():
-    db.create_all()
 
 @main.route('/')
 def home():
@@ -16,7 +13,9 @@ def home():
 
 @main.route('/login')
 def login():
-    return current_app.auth0.authorize_redirect(redirect_uri=current_app.config['AUTH0_CALLBACK_URL'])
+    return current_app.auth0.authorize_redirect(
+        redirect_uri=current_app.config['AUTH0_CALLBACK_URL']
+    )
 
 @main.route('/register')
 def register():
@@ -33,26 +32,28 @@ def callback():
         return redirect(url_for('main.home'))
 
     userinfo = current_app.auth0.parse_id_token(token)
-    sub, name, email, picture = (
-        userinfo['sub'],
-        userinfo.get('name'),
-        userinfo.get('email'),
-        userinfo.get('picture')
-    )
+    sub     = userinfo['sub']
+    name    = userinfo.get('name')
+    email   = userinfo.get('email')
+    picture = userinfo.get('picture')
 
-    # Upsert user into local DB
+    # Upsert into local users table
     user = User.query.filter_by(sub=sub).first()
     if not user:
         user = User(sub=sub, name=name, email=email, picture=picture)
         db.session.add(user)
     else:
-        user.name = name
-        user.email = email
+        user.name    = name
+        user.email   = email
         user.picture = picture
     db.session.commit()
 
-    # Store minimal info in session
-    session['user'] = {'sub': sub, 'name': name, 'email': email, 'picture': picture}
+    session['user'] = {
+        'sub': sub,
+        'name': name,
+        'email': email,
+        'picture': picture
+    }
     return redirect(url_for('main.home'))
 
 @main.route('/logout')
